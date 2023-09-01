023dd44be8e2a554fa0dd5ae15375bfa89964d9a
Thomas Luechtefeld <tom@insilica.co>
2023-09-01 12:33:45 +0000

    updated styling a bit

--- services/report/model/reports/reprotox.py
+++ services/report/model/reports/reprotox.py
@@ -1,31 +1,41 @@
 import requests, pdfkit, flask, json
+from rdkit import Chem
+
+def inchi2smi(inchi):
+    mol = Chem.MolFromInchi(inchi)
+    smiles = Chem.MolToSmiles(mol)
+    return smiles
 
 class ReprotoxReport:
-    def __init__(self, prediction, analogue_info):
+
+    def __init__(self, inchi, prediction, analogue_info):
+        self.inchi = inchi
+        self.smiles = inchi2smi(inchi)
         self.prediction = prediction
         self.analogue_info = analogue_info
 
     @classmethod
     def from_inchi(cls, inchi):
-        # API call logic
-        # api_url = f"http://localhost:6500/predict?inchi={inchi}&label=25&k=2"
-        # response = requests.get(api_url)
-        # api_data = response.json()
 
-        response = """{"weights": [1.0000001192092896, 0.6444193720817566, 0.6379547119140625, 0.6308428049087524, 0.6285170912742615], "analogue_smiles": ["CC(=O)Oc1ccccc1C(=O)O", "COC(=O)c1cccc(C(=O)OC)c1", "O=C(OCc1ccccc1)c1ccccc1", "O=C(OCc1ccccc1)c1ccccc1O", "CC(C=O)Cc1ccc(C(C)C)cc1"], "analogue_values": [0.0, 0.0, 0.0, 0.0, 0.0], "prediction": 0.0}"""
-        api_data = json.loads(response)
-
+        # TODO need a model service
+        # TODO need to update cvae with reprotox labels
+        url = "https://api.insilica.co/service/run/chemsim/predict"
+        params = { "inchi": inchi, "label": "25", "k": 5 }
+        data = json.loads(requests.get(url,params).text)
+        
+        # Accessing specific keys in the parsed data
+        weights = data['weights']
+        analogue_smiles = data['analogue_smiles']
+        analogue_values = data['analogue_values']
+        prediction = data['prediction']
+        
         analogue_info = []
 
-        for weight, smile, value in zip(api_data['weights'], api_data['analogue_smiles'], api_data['analogue_values']):
-            analogue = {
-                'weight': weight,
-                'smile': smile,
-                'value': value
-            }
+        for weight, smile, value in zip(weights, analogue_smiles, analogue_values):
+            analogue = { 'weight': weight, 'smile': smile, 'value': value}
             analogue_info.append(analogue)
 
-        return cls(api_data['prediction'], analogue_info)
+        return cls(inchi, prediction, analogue_info)
 
     def generate_html_content(self, template_path):
         # Render HTML content using the given template path and this object's data
