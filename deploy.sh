c93d9d43a54b6a4a4e1479633d419e04484c6c9c
Thomas Luechtefeld <tom@insilica.co>
2024-02-15 14:39:05 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# zip the entire directory
+zip -r deploy.zip .
+
+# scp the zip file to the server
+scp deploy.zip toxindex:/home/ubuntu
+
+# ssh into the server
+ssh toxindex
+
+# stop all running containers
+docker stop $(docker ps -a -q)
+
+# mv and unzip depoy.zip
+mv ~/deploy.zip /mnt/ebs/toxindex
+cd /mnt/ebs
+
+
+# unzip the file to /mnt/toxindex
+unzip deploy.zip 
