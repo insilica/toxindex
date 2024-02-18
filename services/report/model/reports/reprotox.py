77089a2b2ecd19d02cad519b2168793f86f899b3
Thomas Luechtefeld <tom@insilica.co>
2024-02-18 18:33:20 -0500

    services working

--- services/report/model/reports/reprotox.py
+++ services/report/model/reports/reprotox.py
@@ -5,6 +5,8 @@ from report import s3store
 import requests
 import sqlite3
 import numpy as np
+import pandas as pd
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def inchi2smi(inchi):
     mol = Chem.MolFromInchi(inchi)
@@ -15,57 +17,61 @@ class CVAEModel:
     
     @staticmethod
     def predict(inchi, property_token):
-        url = "http://localhost:6515/predict"
+        url = "https://api.insilica.co/service/run/chemsim/predict"
         params = {'inchi': inchi, 'property_token': property_token}
         response = requests.get(url, params=params)
-        return response.json()  # Assuming the response is JSON-formatted
+        return response.json()
 
-import pandas as pd
-cvaesql = sqlite3.connect('report/data/cvae.sqlite')
-# inchi = 'InChI=1S/C9H8O4/c1-6(10)13-8-5-3-2-4-7(8)9(11)12/h2-5H,1H3,(H,11,12)'
-def repro_report(inchi):
-    
-    # Build the property table for inchi =========================================================
-    query = f"""
-    SELECT * FROM property p 
-    INNER JOIN property_category pc ON p.property_id = pc.property_id
-    INNER JOIN category c ON pc.category_id = c.category_id
-    """    
-    property = pd.read_sql_query(query, cvaesql)
-    query = f"SELECT property_token, value FROM activity where inchi = '{inchi}' AND value='positive'"
-    activity = pd.read_sql_query(query, cvaesql)
-    property = property.merge(activity, on='property_token', how='left')
-    property['value'] = property['value'].fillna('unknown')
-    
-    # Predict properties for reprotox =========================================================
-    repro = property[(property['category'] == "reproductive toxicity") & (property['strength'] > 7)]['property_token']
-    devtox = property[(property['category'] == "developmental toxicity") & (property['strength'] > 7)]['property_token']
-    endo = property[(property['category'] == "endocrine disruption") & (property['strength'] > 8)]['property_token']
-    
-    property_tokens = np.unique(list(repro) + list(devtox) + list(endo))
-    
-    ## generate predictions
-    pdf = pd.DataFrame({'property_token': property_tokens, 'prediction': np.nan})
-    for i, property_token in enumerate(pdf['property_token']):
-        prediction = CVAEModel.predict(inchi, property_token)['positive_prediction']
-        pdf.at[i, 'prediction'] = prediction
-    
-    # put it together
-    output = pdf.merge(property, on='property_token')
-    return output[['title','prediction','category','prediction','value']]
 
-    
-    
 class ReprotoxReport:
 
-    def __init__(self, title, inchi):
+    jinja = Environment(
+        loader=FileSystemLoader('report/templates/reports'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    def __init__(self, title, inchi, prediction_df):
         self.title = title
         self.inchi = inchi
-        self.smiles = inchi2smi(inchi)   
-        
+        self.smiles = inchi2smi(inchi)
+        self.prediction_df = prediction_df
+        self.generated_on = pd.Timestamp.now(tz='UTC')
+    
+    # inchi = 'InChI=1S/C9H8O4/c1-6(10)13-8-5-3-2-4-7(8)9(11)12/h2-5H,1H3,(H,11,12)'
     @staticmethod
-    def from_json(json):
-        return ReprotoxReport(json['title'], json['inchi'])
+    def _generate_predictions(inchi):
+        cvaesql = sqlite3.connect('report/data/cvae.sqlite')
+        # Build the property table for inchi =========================================================
+        query = f"""
+        SELECT * FROM property p 
+        INNER JOIN property_category pc ON p.property_id = pc.property_id
+        INNER JOIN category c ON pc.category_id = c.category_id
+        WHERE strength > 8
+        """    
+        property = pd.read_sql_query(query, cvaesql)
+        query = f"SELECT property_token, value FROM activity where inchi = '{inchi}' AND value='positive'"
+        activity = pd.read_sql_query(query, cvaesql)
+        property = property.merge(activity, on='property_token', how='left')
+        property['value'] = property['value'].fillna('unknown')
+        
+        # Predict properties for reprotox =========================================================
+        repro = property[(property['category'] == "reproductive toxicity")]['property_token']
+        devtox = property[(property['category'] == "developmental toxicity")]['property_token']
+        endo = property[(property['category'] == "endocrine disruption") & (property['strength'] > 9)]['property_token']
+        
+        property_tokens = np.unique(list(repro) + list(devtox) + list(endo))
+        
+        ## generate predictions
+        pdf = pd.DataFrame({'property_token': property_tokens, 'prediction': np.nan})
+        for i, property_token in enumerate(pdf['property_token']):
+            logging.info(f"Predicting property {property_token}")
+            prediction = CVAEModel.predict(inchi, property_token)['positive_prediction']
+            pdf.at[i, 'prediction'] = prediction
+        
+        # put it together
+        output = pdf.merge(property, on='property_token')
+        cvaesql.close()
+        return output[['title','prediction','category','prediction','value']]
     
     @classmethod
     def generate(cls, report_id, title, inchi):
@@ -73,8 +79,17 @@ class ReprotoxReport:
         
         s3 = s3store.S3Store()
         
-        report = ReprotoxReport(title, inchi)
-        html_content = flask.render_template("reports/reprotox.html", report=report)
+        predictions = cls._generate_predictions(inchi)
+        report = ReprotoxReport(title, inchi, predictions)
+        group_preds = report.prediction_df.groupby('category').apply(lambda x: x.to_dict(orient='records')).to_dict()
+        report_data = {
+            "title": report.title,
+            "generated_on": report.generated_on.strftime('%Y-%m-%d %H:%M'),
+            "inchi": report.inchi,
+            "smiles": report.smiles
+        }
+        template = cls.jinja.get_template('reprotox.html')
+        html_content = template.render(report=report_data, grouped_data = group_preds)
 
         config = pdfkit.configuration(wkhtmltopdf="/usr/bin/wkhtmltopdf")
         
