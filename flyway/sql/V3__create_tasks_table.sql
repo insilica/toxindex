b2318cc64739b6022b5bc8b26d3a051456ae72d8
Thomas Luechtefeld <tom@insilica.co>
2025-05-20 01:22:36 +0000

    basic celery task working

--- flyway/sql/V3__create_tasks_table.sql
+++ flyway/sql/V3__create_tasks_table.sql
@@ -2,7 +2,9 @@ CREATE TABLE tasks (
     task_id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    user_id INTEGER REFERENCES users(user_id),
+    user_id UUID REFERENCES users(user_id),
     workflow_id INTEGER REFERENCES workflows(workflow_id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    celery_task_id TEXT
 );
--- flyway/sql/V4__create_messages_table.sql
+++ flyway/sql/V4__create_messages_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE messages (
     message_id SERIAL PRIMARY KEY,
     task_id INTEGER REFERENCES tasks(task_id),
-    user_id INTEGER REFERENCES users(user_id),
+    user_id UUID REFERENCES users(user_id),
     role VARCHAR(50) NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
