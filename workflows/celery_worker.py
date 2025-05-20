86a2e38cf035d7e04ce217f187be48846b3998a4
Thomas Luechtefeld <tom@insilica.co>
2025-05-20 01:22:36 +0000

    basic celery task working

---
+++ workflows/celery_worker.py
@@ -0,0 +1,15 @@
+from celery import Celery
+
+
+def make_celery(app_name=__name__):
+    return Celery(
+        app_name,
+        broker='redis://localhost:6379/0',
+        backend='redis://localhost:6379/0',
+    )
+
+celery = make_celery()
+# celery -A workflows.celery_worker.celery worker --loglevel=info
+
+# task modules
+import workflows.probra as probra_task
