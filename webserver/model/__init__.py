7fa779a599159787cd5c647ff29d1bdf88f10f05
Thomas Luechtefeld <tom@insilica.co>
2025-05-25 01:20:02 +0000

    sockets working

--- webserver/model/__init__.py
+++ webserver/model/__init__.py
@@ -1,5 +1,7 @@
 from webserver.model.task import Task
 from webserver.model.user import User
 from webserver.model.workflow import Workflow
+from webserver.model.message import Message
+from webserver.model.file import File
 
 __all__ = ['Task', 'User', 'Workflow']
--- webserver/model/task.py
+++ webserver/model/task.py
@@ -3,11 +3,13 @@ import logging
 import datetime
 
 class Task:
-    def __init__(self, task_id, title, user_id, workflow_id, description=None, created_at=None):
+
+    def __init__(self, task_id, title, user_id, workflow_id, celery_task_id=None, description=None, created_at=None):
         self.task_id = task_id
         self.title = title
         self.user_id = user_id
         self.workflow_id = workflow_id
+        self.celery_task_id = celery_task_id
         self.description = description
         self.created_at = created_at
 
@@ -28,15 +30,16 @@ class Task:
             title=row['title'],
             user_id=row['user_id'],
             workflow_id=row['workflow_id'],
+            celery_task_id=row['celery_task_id'],
             description=row.get('description'),
             created_at=row['created_at']
         )
 
     @staticmethod
-    def create_task(title, user_id, workflow_id, description=None):
+    def create_task(title, user_id, workflow_id, celery_task_id=None, description=None):
         logging.info(f"Creating new task with title='{title}' for user_id={user_id}")
-        params = (title, user_id, workflow_id, description)
-        ds.execute("INSERT INTO tasks (title, user_id, workflow_id, description) VALUES (%s, %s, %s, %s)", params)
+        params = (title, user_id, celery_task_id, workflow_id, description)
+        ds.execute("INSERT INTO tasks (title, user_id, celery_task_id, workflow_id, description) VALUES (%s, %s, %s, %s, %s)", params)
         
         # Fetch and return the newly created task
         res = ds.find("SELECT * FROM tasks WHERE title = %s AND user_id = %s ORDER BY created_at DESC LIMIT 1",
@@ -54,8 +57,13 @@ class Task:
         return [Task.from_row(row) for row in rows]
     
     @staticmethod
-    def get_task(task_id, user_id):
-        res = ds.find("SELECT * FROM tasks WHERE task_id = %s AND user_id = %s", (task_id, user_id))
+    def get_tasks_by_celery_task_id(celery_task_id):
+        res = ds.find("SELECT * FROM tasks WHERE celery_task_id = %s", (celery_task_id,))
+        return Task.from_row(res) if res else None
+    
+    @staticmethod
+    def get_task(task_id):
+        res = ds.find("SELECT * FROM tasks WHERE task_id = %s", (task_id,))
         return Task.from_row(res) if res else None
 
     @staticmethod
@@ -63,7 +71,13 @@ class Task:
         params = (task_id, user_id, role, content)
         ds.execute("INSERT INTO messages (task_id, user_id, role, content) VALUES (%s, %s, %s, %s)", params)
         return True
-
+    
+    @staticmethod
+    def update_celery_task_id(task_id, celery_task_id):
+        params = (celery_task_id, task_id)
+        ds.execute("UPDATE tasks SET celery_task_id = %s WHERE task_id = %s", params)
+        return True
+    
     @staticmethod
     def get_messages(task_id, user_id):
         # Check if the task belongs to the user
