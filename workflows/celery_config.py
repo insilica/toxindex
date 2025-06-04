c57e6f4301360b46567cd9a553cea48257fa36f3
Kyu Han <kyu@insilica.co>
2025-06-04 16:10:47 -0400

    fixed postgres and added result

--- workflows/celery_config.py
+++ workflows/celery_config.py
@@ -2,6 +2,7 @@ broker_url = 'redis://localhost:6379/0'
 result_backend = 'redis://localhost:6379/0'
 broker_connection_retry_on_startup = True
 worker_max_tasks_per_child = 1000
+worker_hijack_root_logger = False
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
