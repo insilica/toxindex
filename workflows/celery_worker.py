70dd145d3d95b001ffda036cee7823add545dfd0
Kyu Han <kyu@insilica.co>
2025-06-04 16:10:47 -0400

    fixed postgres and added result

--- workflows/celery_worker.py
+++ workflows/celery_worker.py
@@ -1,3 +1,28 @@
+import logging
+import os
+from datetime import datetime
+
+# BD ENC
+print("PGUSER:", os.getenv("PGUSER"))
+print("PGHOST:", os.getenv("PGHOST"))
+print("PGPORT:", os.getenv("PGPORT"))
+print("PGDATABASE:", os.getenv("PGDATABASE"))
+print("PG_SOCKET_DIR:", os.getenv("PG_SOCKET_DIR"))
+
+# Ensure logs directory exists
+os.makedirs(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'logs')), exist_ok=True)
+log_filename = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', 'logs', f'app_{datetime.now().strftime("%Y-%m-%d_%H")}.log'))
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s',
+    handlers=[
+        logging.FileHandler(log_filename),
+        logging.StreamHandler()
+    ]
+)
+
 from celery import Celery
 from . import celery_config
 
