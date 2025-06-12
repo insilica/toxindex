2ccb6e8ca18b2229147238d734d297618fafa757
Kyu Han <kyu@insilica.co>
2025-06-12 16:37:22 -0400

    fixfly

--- flyway/sql/V8__cascade_delete_environments.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE tasks DROP CONSTRAINT tasks_environment_id_fkey;
-ALTER TABLE tasks
-ADD CONSTRAINT tasks_environment_id_fkey
-FOREIGN KEY (environment_id) REFERENCES environments(environment_id) ON DELETE CASCADE; 
